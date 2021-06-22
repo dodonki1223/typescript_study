efb3bd0670a3b24a5e26ce66f2fa04481792c753
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-06-22 09:53:30 +0900

    :sparkles: 2章:作りながら学ぶTypeScript - JavaScriptで発生しうる問題 - 1

--- yytypescript/02_welcome_typescript/increment.ts
+++ yytypescript/02_welcome_typescript/increment.ts
@@ -1,13 +1,16 @@
 /*
     JavaScriptで発生しうる問題に対応したTypeScriptの書きぶり
       https://book.yyts.org/handson/make-a-simple-function-via-cli#javascriptdeshiuru
-      引数と返り値に型情報を設定することで正しい値で返すことができるようになる
+      「引数」と「返り値」に型情報を設定することで正しい値で返すことができるようになる
+      戻り値は型推論に任せて書かないほうが良いという考え方もある
  */
-function increment(num) {
-    return num + 1;
+function increment(num: number): number{
+  return num + 1;
 }
+
 // TypeScriptで記述すると以下のようなエラーがでるようになります
 // 型 'string' の引数を型 'number' のパラメーターに割り当てることはできません。ts(2345)
 // console.log(increment('1'));
+
 // これで問題ない！
 console.log(increment(1));
